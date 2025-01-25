ae8c705a33f6230f6df842714793802105e22441
Pranav Batra <pranav.batra2006@gmail.com>
2025-01-24 22:22:39 -0800

    cleanup

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('create_profile.html')
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    return render_template('create_profile.html')
