5d4528c7b7f3076a3df6f99ed0e9b502362c3e47
Jiwon Hong <jiwonh11@uci.edu>
2025-01-24 21:47:15 -0800

    added bubble box

--- zot-gym/src/components/bubble.js
+++ zot-gym/src/components/bubble.js
@@ -1,9 +1,18 @@
-export default function Bubble(props) {
-  const { is_sender, message } = props;
+import React from 'react';
+import styles from 'zot-gym/src/styles/bubble.css';
+
+
+export default function Bubble({ isSender, message }) {
+  const bubbleClass = isSender ? `${styles.messageBox} ${styles.sender}` : `${styles.messageBox} ${styles.receiver}`;
+
   return (
-    <>
-        <h1>{message}</h1>
-    </>
-  )
+    <div className={bubbleClass}>
+      <p>{message}</p>
+    </div>
+  );
 }
 
+Bubble.propTypes = {
+  isSender: PropTypes.bool.isRequired,
+  message: PropTypes.string.isRequired,
+};
